c7d9f476d008daf8011d885ed56fa259cd636c3b
Edson Costa <edson@onlogexpress.com.br>
2022-02-05 00:56:27 -0300

    outros1

--- index.php
+++ index.php
@@ -1,48 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Deploy com GitHub Actions</title>
-    <link
-      href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
-  </head>
-  <body class="bg-gray-800">
-    <div
-      class="
-        container
-        mx-auto
-        max-w-sm
-        h-screen
-        flex flex-col
-        space-y-4
-        justify-center
-        items-center
-      "
-    >
-      <div
-        class="bg-white w-full flex items-center p-2 rounded-xl shadow border"
-      >
-        <div class="relative flex items-center space-x-4">
-          <img
-            src="./36087036_1954869781470355_898582861416759296_n.jpg"
-            alt="Edson Costa"
-            class="w-16 h-16 rounded-full"
-          />
-        </div>
-        <div class="flex-grow p-3">
-          <div class="font-semibold text-gray-700">Janaina Ribeiro</div>
-          <div class="text-sm text-gray-500">@janarsilva</div>
-        </div>
-        <div class="p-2">
-          <span
-            class="block h-4 w-4 bg-green-400 rounded-full bottom-0 right-0"
-          ></span>
-        </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Deploy com GitHub Actions</title>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+</head>
+
+<body class="bg-gray-800">
+  <div class="container mx-auto max-w-sm h-screen flex flex-col space-y-4 justify-center items-center ">
+    <div class="bg-white w-full flex items-center p-2 rounded-xl shadow border">
+      <div class="relative flex items-center space-x-4">
+        <img src="./60551398_2165399503750714_6938679714349318144_n.jpg" alt="Edson Costa" class="w-16 h-16 rounded-full" />
+      </div>
+      <div class="flex-grow p-3">
+        <div class="font-semibold text-gray-700">Janaina Ribeiro</div>
+        <div class="text-sm text-gray-500">@janarsilva</div>
+      </div>
+      <div class="p-2">
+        <span class="block h-4 w-4 bg-green-400 rounded-full bottom-0 right-0"></span>
       </div>
     </div>
-  </body>
+
+    <div class="bg-white w-full flex items-center p-2 rounded-xl shadow border">
+      <div class="relative flex items-center space-x-4">
+        <img src="./36087036_1954869781470355_898582861416759296_n.jpg" alt="Edson Costa" class="w-16 h-16 rounded-full" />
+      </div>
+      <div class="flex-grow p-3">
+        <div class="font-semibold text-gray-700">Edson Costa</div>
+        <div class="text-sm text-gray-500">@edsonrcoosta</div>
+      </div>
+      <div class="p-2">
+        <span class="block h-4 w-4 bg-blue-400 rounded-full bottom-0 right-0"></span>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
